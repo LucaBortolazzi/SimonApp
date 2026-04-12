42e8f8e589b6fd2954a4620ad78032a3b33f5c70
Luca <luca.bortolazzi@studenti.unipd.it>
2026-04-12 12:20:32 +0200

    gestione navigator

--- app/src/main/java/com/example/simonapp/MainActivity.kt
+++ app/src/main/java/com/example/simonapp/MainActivity.kt
@@ -11,37 +11,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.simonapp.ui.theme.SimonAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             SimonAppTheme {
+                val navController = rememberNavController()     //utilizzo navigation
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavHost(
+                        navController = navController,
+                        startDestination = "Gameplay",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ){
+                        composable("Gameplay"){
+                            GameplayScreen(
+                                onCancelClicked = {navController.navigate("GameHistory")}
+                            )
+                        }
+
+                        composable("GameHistory"){
+                            GameHistoryScreen()
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SimonAppTheme {
-        Greeting("Android")
-    }
+    SimonAppTheme {}
 }

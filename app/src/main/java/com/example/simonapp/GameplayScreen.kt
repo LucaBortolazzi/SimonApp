0123e71830f47793ce41d5a4ef64fa9c1d4fd0b2
Luca <luca.bortolazzi@studenti.unipd.it>
2026-04-13 00:17:43 +0200

    ultimazione prima schermata

--- app/src/main/java/com/example/simonapp/GameplayScreen.kt
+++ app/src/main/java/com/example/simonapp/GameplayScreen.kt
@@ -1,15 +1,25 @@
 package com.example.simonapp
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
@@ -22,50 +32,187 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
+
+/*
+* guarda font
+* dimensione bottoni sotto
+* linee separatrici tra elementi
+*/
 
 val colorList = listOf(
-    "R" to Color.Red,
-    "G" to Color.Green,
-    "B" to Color.Blue,
-    "M" to Color.Magenta,
-    "Y" to Color.Yellow,
-    "C" to Color.Cyan
+    'R' to Color.Red,
+    'G' to Color.Green,
+    'B' to Color.Blue,
+    'M' to Color.Magenta,
+    'Y' to Color.Yellow,
+    'C' to Color.Cyan
 )
 
 @Composable
 fun ColorsGrid(
-    onButtonPressed: () -> Unit,
+    onButtonPressed: (Char) -> Unit,    //comunica quale lettera premuta
     modifier: Modifier = Modifier
 ){
     LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        modifier = modifier
+        columns = GridCells.Fixed(2),   //6 elementi
+        modifier = modifier.fillMaxSize()
     ){
-        items(/*lista di colori ?*/){  //sistemare!!!
+        items(colorList){ (letter, color) ->    //pair lettera, colore
+            ColoredButton(
+                letter = letter,
+                color = color,
+                onClick = { onButtonPressed(letter) }
+            )
 
         }
     }
 }
 
-//bottoni colorati
-
-//sequenza colori
-
 @Composable
-fun GameplayScreen(onEndGameClicked: () -> Unit){
+fun ColoredButton(
+    color: Color,
+    letter: Char,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+){
     val orientation = LocalConfiguration.current.orientation
 
-    var sequence by rememberSaveable { mutableStateOf(listOf<String>()) }
+    //gestione dimensione bottoni in base ad orientazione
+    val ratio = if (orientation == Configuration.ORIENTATION_LANDSCAPE) 2.5f else 1f
 
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(containerColor = color),
+        shape = RoundedCornerShape(12.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(6.dp)
+            .aspectRatio(ratio)
+    ){
+        Text(
+            text = letter.toString(),   //serve string per Text
+            fontSize = 16.sp,
+            color = Color.Gray,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun SequenceLetter(
+    sequence: List<Char>,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        //trasforma elementi di Lista in stringa con virgola che separa
+        text = sequence.joinToString(", "),
+        fontSize = 16.sp,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    )
+}
+
+@Composable
+fun ButtonsRow(
+    onClearClicked: () -> Unit,
+    onEndGameClicked: (List<Char>) -> Unit,
+    sequence: List<Char>,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(onClick = onClearClicked) {
+            Text(text = stringResource(R.string.btn_clear))
+        }
+        Button(onClick = { onEndGameClicked (sequence)} ) {
+            Text(text = stringResource(R.string.btn_endGame))
+        }
+    }
+}
+
+@Composable
+fun GameplayScreen(
+    onEndGameClicked: (List<Char>) -> Unit,
+    modifier: Modifier = Modifier
+){
+    //ritorna configurazione corrente
+    val orientation = LocalConfiguration.current.orientation
+
+    //sequenza sopravvive a cambi di configurazione
+    var sequence by rememberSaveable { mutableStateOf(listOf<Char>()) }
+
+    //layout in base a orientazione corrente
     if(orientation == Configuration.ORIENTATION_LANDSCAPE){
 
         //LANDSCAPE
 
+        Row(modifier = modifier.fillMaxSize()) {
+            ColorsGrid(
+                //aggiungo alla sequenza la lettera corrispondente al colore premuto
+                onButtonPressed = { letter -> sequence = sequence + letter },
+                modifier = Modifier
+                    .weight(1.5f)   //60% dello schermo
+                    .fillMaxHeight()
+            )
+            Column(
+                modifier = Modifier
+                    .weight(1f)     //40% rimanente dello schermo
+                    .fillMaxHeight()
+                    .padding(8.dp)
+            ){
+                //margine superiore
+                Spacer(modifier = Modifier.height(40.dp))
+
+                SequenceLetter(
+                    sequence = sequence,
+                    modifier = Modifier.weight(1f)
+                )
+
+                ButtonsRow(
+                    sequence = sequence,
+                    onClearClicked = { sequence = emptyList() },
+                    onEndGameClicked = {
+                        onEndGameClicked(sequence)      //sequenza mandata a GameHistoryScreen
+                        sequence = emptyList()          //azzera sequenza
+                    }
+                )
+            }
+        }
     }
     else{
 
         //PORTRAIT
 
+        Column(modifier = modifier.fillMaxSize()) {
+            ColorsGrid(
+                onButtonPressed = { letter -> sequence = sequence + letter },
+                modifier = Modifier
+                    .weight(2f)     //80% schermo
+                    .fillMaxWidth()
+            )
+
+            SequenceLetter(
+                sequence = sequence,
+                modifier = Modifier.weight(0.4f)    //20% schermo
+            )
+
+            ButtonsRow(
+                sequence = sequence,
+                onClearClicked = { sequence = emptyList() },
+                onEndGameClicked = {
+                    onEndGameClicked(sequence)      //sequenza mandata a GameHistoryScreen
+                    sequence = emptyList()          //azzera sequenza
+                }
+            )
+        }
     }
 }
